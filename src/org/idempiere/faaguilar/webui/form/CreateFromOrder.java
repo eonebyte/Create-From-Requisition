eef1501147d97decf69ab08f130b0cf4dd0cca7a
eonebyte <eonebyte@gmail.com>
2026-05-25 12:59:57 +0700

    Improve: Adapted for iDempiere 13, Java 17 & ZK 10.x

--- src/org/idempiere/faaguilar/webui/form/CreateFromOrder.java
+++ src/org/idempiere/faaguilar/webui/form/CreateFromOrder.java
@@ -37,222 +37,153 @@ import org.compiere.util.Msg;
 /**
  * 
  * @author Fabian Aguilar faaguilar@gmail.com
- *
+ * @author Ahmad Fauzi Ridwan @eonebyte (Adapted for iDempiere 13 / Java 17)
  */
-public abstract class CreateFromOrder extends CreateFrom
-{
-	/**
-	 *  Protected Constructor
-	 *  @param mTab MTab
-	 */
-	public CreateFromOrder(GridTab mTab)
-	{
-		super(mTab);
-		if (log.isLoggable(Level.INFO)) log.info(mTab.toString());
-	}   //  VCreateFromInvoice
+public abstract class CreateFromOrder extends CreateFrom {
 
-	/**
-	 *  Dynamic Init
-	 *  @return true if initialized
-	 */
-	public boolean dynInit() throws Exception
-	{
-		log.config("");
-		setTitle(Msg.getElement(Env.getCtx(), "C_Order_ID", false) + " .. " + Msg.translate(Env.getCtx(), "CreateFrom"));
+    public CreateFromOrder(GridTab mTab) {
+        super(mTab);
+        if (log.isLoggable(Level.INFO))
+            log.info(mTab.toString());
+    }
 
-		return true;
-	}   //  dynInit
+    public boolean dynInit() throws Exception {
+        log.config("");
+        setTitle(Msg.getElement(Env.getCtx(), "C_Order_ID", false)
+                + " .. " + Msg.translate(Env.getCtx(), "CreateFrom"));
+        return true;
+    }
 
+    protected Vector<Vector<Object>> getRequisitionData(Object Requisition, Object Org, Object User) {
+        Vector<Vector<Object>> data = new Vector<>();
+        StringBuilder sql = new StringBuilder(
+                "SELECT r.M_Requisition_ID, r.DocumentNo, r.DateRequired, r.PriorityRule, rl.M_Product_ID,")
+                .append(" p.Name AS ProductName, rl.Description, rl.Qty, rl.C_BPartner_ID, bp.Name AS BpName,")
+                .append(" rl.M_RequisitionLine_ID, u.Name AS Username, o.Name AS OrgName,")
+                .append(" c.C_Charge_ID, c.name AS ChargeName")
+                .append(" FROM M_Requisition r")
+                .append(" INNER JOIN M_RequisitionLine rl ON (r.M_Requisition_ID = rl.M_Requisition_ID)")
+                .append(" INNER JOIN AD_User u ON (r.AD_User_ID = u.AD_User_ID)")
+                .append(" INNER JOIN AD_Org o ON (r.AD_Org_ID = o.AD_Org_ID)")
+                .append(" LEFT OUTER JOIN M_Product p ON (rl.M_Product_ID = p.M_Product_ID)")
+                .append(" LEFT OUTER JOIN C_Charge c ON (rl.C_Charge_ID = c.C_Charge_ID)")
+                .append(" LEFT OUTER JOIN C_BPartner bp ON (rl.C_BPartner_ID = bp.C_BPartner_ID)")
+                .append(" WHERE r.DocStatus = 'CO' AND rl.C_OrderLine_ID IS NULL");
 
-	/**
-	 *  Load Data - Shipment not invoiced
-	 *  @param M_InOut_ID InOut
-	 */
-	protected Vector<Vector<Object>> getRequisitionData(Object Requisition, Object Org,  Object User)
-	{
-		
+        if (Requisition != null)
+            sql.append(" AND rl.M_Requisition_ID = ?");
+        if (Org != null)
+            sql.append(" AND r.AD_Org_ID = ?");
+        if (User != null)
+            sql.append(" AND r.AD_User_ID = ?");
 
-		//
-		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-		StringBuilder sql = new StringBuilder("select r.M_Requisition_ID,r.DocumentNo,r.DateRequired,r.PriorityRule,rl.M_Product_ID,");   //1-5
-				sql.append(" p.Name as ProductName,rl.Description,rl.Qty,rl.C_BPartner_ID, bp.Name as BpName, rl.M_RequisitionLine_ID, u.Name as Username, o.Name as OrgName,");  //6-13
-				sql.append(" c.C_Charge_ID, c.name as ChargeName");// 14-15
-				sql.append(" from M_Requisition r " );
-				sql.append(" inner join M_RequisitionLine rl on (r.m_requisition_id=rl.m_requisition_id)" );
-				sql.append(" inner join AD_User u on (r.AD_User_ID=u.AD_User_ID)" );
-				sql.append(" inner join AD_Org o on (r.AD_Org_ID=o.AD_Org_ID)" );
-				sql.append(" left outer join M_Product p on (rl.M_Product_ID=p.M_Product_ID)" );
-				sql.append(" left outer join C_Charge c on (rl.C_Charge_ID=c.C_Charge_ID)" );
-				sql.append(" left outer join C_BPartner bp on (rl.C_BPartner_ID=bp.C_BPartner_ID)" );
-				sql.append(" where r.docstatus='CO' and rl.C_OrderLine_ID is null");
-		
-		if(Requisition!=null)
-			sql.append(" AND rl.M_Requisition_ID=?");
-		if(Org!=null)
-			sql.append(" AND r.AD_Org_ID=?");
-		if(User!=null)
-			sql.append(" AND r.AD_User_ID=?");
-		
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try
-		{
-			int i=1;
-			pstmt = DB.prepareStatement(sql.toString(), null);
-			if(Requisition!=null)
-				pstmt.setInt(i++, (Integer)Requisition);
-			if(Org!=null)
-				pstmt.setInt(i++, (Integer)Org);
-			if(User!=null)
-				pstmt.setInt(i++, (Integer)User);
-			rs = pstmt.executeQuery();
-			while (rs.next())
-			{
-				Vector<Object> line = new Vector<Object>(7);
-				line.add(new Boolean(false));           //  0-Selection
-				line.add(rs.getString(13)); //1-OrgName
-				
-				KeyNamePair pp =  new KeyNamePair(rs.getInt(11), rs.getString(2).trim());
-				line.add(pp);  //  2-DocumentNo Line iD
-				line.add(rs.getTimestamp(3));//  3-DateRequired
-				
-				if(rs.getString(10)!=null)
-				{
-					pp = new KeyNamePair(rs.getInt(9), rs.getString(10).trim());
-					line.add(pp);	//  4-BPartner
-				}
-				else
-					line.add(null); //  4-BPartner
-			
-				if( rs.getString(6)!=null)
-				{
-				pp = new KeyNamePair(rs.getInt(5), rs.getString(6).trim());
-				line.add(pp);				// 5-Product
-				}
-				else
-				line.add(null);      // 5-Product
-				
-				if(rs.getString(15)!=null)
-					line.add(rs.getString(15)); //6-charge
-				else
-					line.add(null); //6-charge
-				
-				line.add(rs.getBigDecimal(8));//7- Qty
-		
-				line.add(rs.getString(7));                           //  8-description
-				line.add(rs.getString(12).trim());                     	//  9-user
-				data.add(line);
-			}
-		}
-		catch (SQLException e)
-		{
-			log.log(Level.SEVERE, sql.toString(), e);
-		}
-		finally
-		{
-			DB.close(rs, pstmt);
-			rs = null;
-			pstmt = null;
-		}
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            int i = 1;
+            pstmt = DB.prepareStatement(sql.toString(), null);
+            if (Requisition != null)
+                pstmt.setInt(i++, (Integer) Requisition);
+            if (Org != null)
+                pstmt.setInt(i++, (Integer) Org);
+            if (User != null)
+                pstmt.setInt(i++, (Integer) User);
 
-		return data;
-	}   //  loadShipment
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Vector<Object> line = new Vector<>();
+                line.add(Boolean.FALSE); // 0-Selection
+                line.add(rs.getString(13)); // 1-OrgName
 
+                KeyNamePair pp = new KeyNamePair(rs.getInt(11), rs.getString(2).trim());
+                line.add(pp); // 2-DocumentNo + LineID
+                line.add(rs.getTimestamp(3)); // 3-DateRequired
 
-	/**
-	 *  List number of rows selected
-	 */
-	public void info(IMiniTable miniTable, IStatusBar statusBar)
-	{
+                String bpName = rs.getString(10);
+                line.add(bpName != null
+                        ? new KeyNamePair(rs.getInt(9), bpName.trim())
+                        : null); // 4-BPartner
 
-	}   //  infoInvoice
+                String prodName = rs.getString(6);
+                line.add(prodName != null
+                        ? new KeyNamePair(rs.getInt(5), prodName.trim())
+                        : null); // 5-Product
 
-	protected void configureMiniTable (IMiniTable miniTable)
-	{
-		miniTable.setColumnClass(0, Boolean.class, false);      //  0-Selection
-		miniTable.setColumnClass(1, String.class, true);        //  1-OrgName
-		miniTable.setColumnClass(2, String.class, true);        //  2-DocumentNo
-		miniTable.setColumnClass(3, Timestamp.class, true);        //  3-DateRequired
-		miniTable.setColumnClass(4, String.class, true);        //  4-BPartner
-		miniTable.setColumnClass(5, String.class, true);        //  5-Product
-		miniTable.setColumnClass(6, String.class, true);        //  6-Charge
-		miniTable.setColumnClass(7, BigDecimal.class, true);        //  7-Qty
-		miniTable.setColumnClass(8, String.class, true);        //  8-Description
-		miniTable.setColumnClass(9, String.class, true);        //  9-User
-		
-		//  Table UI
-		miniTable.autoSize();
-	}
+                line.add(rs.getString(15)); // 6-Charge (nullable)
+                line.add(rs.getBigDecimal(8)); // 7-Qty
+                line.add(rs.getString(7)); // 8-Description
+                line.add(rs.getString(12) != null ? rs.getString(12).trim() : ""); // 9-User
+                data.add(line);
+            }
+        } catch (SQLException e) {
+            log.log(Level.SEVERE, sql.toString(), e);
+        } finally {
+            DB.close(rs, pstmt);
+        }
+        return data;
+    }
 
-	/**
-	 *  Save - Create Invoice Lines
-	 *  @return true if saved
-	 */
-	public boolean save(IMiniTable miniTable, String trxName)
-	{
-		//  Order
-		int C_Order_ID = ((Integer)getGridTab().getValue("C_Order_ID")).intValue();
-		MOrder order = new MOrder (Env.getCtx(), C_Order_ID, trxName);
-		if (log.isLoggable(Level.CONFIG)) log.config(order.toString());
+    public void info(IMiniTable miniTable, IStatusBar statusBar) {
+    }
 
-		//  Lines
-		for (int i = 0; i < miniTable.getRowCount(); i++)
-		{
-			if (((Boolean)miniTable.getValueAt(i, 0)).booleanValue())
-			{
-				
+    protected void configureMiniTable(IMiniTable miniTable) {
+        miniTable.setColumnClass(0, Boolean.class, false);
+        miniTable.setColumnClass(1, String.class, true);
+        miniTable.setColumnClass(2, String.class, true);
+        miniTable.setColumnClass(3, Timestamp.class, true);
+        miniTable.setColumnClass(4, String.class, true);
+        miniTable.setColumnClass(5, String.class, true);
+        miniTable.setColumnClass(6, String.class, true);
+        miniTable.setColumnClass(7, BigDecimal.class, true);
+        miniTable.setColumnClass(8, String.class, true);
+        miniTable.setColumnClass(9, String.class, true);
+        miniTable.autoSize();
+    }
 
-				KeyNamePair pp = (KeyNamePair)miniTable.getValueAt(i, 2);   //  1-documentno  line id
-				int M_RequisitionLine_ID = pp.getKey();
-				MRequisitionLine rLine = new MRequisitionLine (Env.getCtx(), M_RequisitionLine_ID, trxName);
+    public boolean save(IMiniTable miniTable, String trxName) {
+        int C_Order_ID = (Integer) getGridTab().getValue("C_Order_ID");
+        MOrder order = new MOrder(Env.getCtx(), C_Order_ID, trxName);
+        if (log.isLoggable(Level.CONFIG))
+            log.config(order.toString());
 
-				//	Create new Order Line
-				MOrderLine m_orderLine = new MOrderLine (order);
-				m_orderLine.setDatePromised(rLine.getDateRequired());
-				if (rLine.getM_Product_ID() >0)
-				{
-					m_orderLine.setProduct(MProduct.get(Env.getCtx(), rLine.getM_Product_ID()));
-					m_orderLine.setM_AttributeSetInstance_ID(rLine.getM_AttributeSetInstance_ID());
-				}
-				else
-				{
-					m_orderLine.setC_Charge_ID(rLine.getC_Charge_ID());
-					
-				}
-				m_orderLine.setPriceActual(rLine.getPriceActual());
-				m_orderLine.setAD_Org_ID(rLine.getAD_Org_ID());
-				m_orderLine.setQty(rLine.getQty());
-				m_orderLine.saveEx();
-				
-				//	Update Requisition Line
-				rLine.setC_OrderLine_ID(m_orderLine.getC_OrderLine_ID());
-				rLine.saveEx();
-				
-			}   //   if selected
-		}   //  for all rows
+        for (int i = 0; i < miniTable.getRowCount(); i++) {
+            if ((Boolean) miniTable.getValueAt(i, 0)) {
+                KeyNamePair pp = (KeyNamePair) miniTable.getValueAt(i, 2);
+                int M_RequisitionLine_ID = pp.getKey();
+                MRequisitionLine rLine = new MRequisitionLine(Env.getCtx(), M_RequisitionLine_ID, trxName);
 
-		
+                MOrderLine orderLine = new MOrderLine(order);
+                orderLine.setDatePromised(rLine.getDateRequired());
+                if (rLine.getM_Product_ID() > 0) {
+                    orderLine.setProduct(MProduct.get(Env.getCtx(), rLine.getM_Product_ID()));
+                    orderLine.setM_AttributeSetInstance_ID(rLine.getM_AttributeSetInstance_ID());
+                } else {
+                    orderLine.setC_Charge_ID(rLine.getC_Charge_ID());
+                }
+                orderLine.setPriceActual(rLine.getPriceActual());
+                orderLine.setAD_Org_ID(rLine.getAD_Org_ID());
+                orderLine.setQty(rLine.getQty());
+                orderLine.saveEx();
 
-		return true;
-	}   //  saveInvoice
+                rLine.setC_OrderLine_ID(orderLine.getC_OrderLine_ID());
+                rLine.saveEx();
+            }
+        }
+        return true;
+    }
 
-	protected Vector<String> getOISColumnNames()
-	{
-		//  Header Info
-	    Vector<String> columnNames = new Vector<String>(7);
-	    columnNames.add(Msg.getMsg(Env.getCtx(), "Select"));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "AD_Org_ID"));
-	    columnNames.add(Msg.translate(Env.getCtx(), "Documentno"));
-	    columnNames.add(Msg.translate(Env.getCtx(), "DateRequired"));
-	    columnNames.add(Msg.translate(Env.getCtx(), "C_BPartner_ID"));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "M_Product_ID", false));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "C_Charge_ID", false));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "Qty"));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "Description", false));
-	    columnNames.add(Msg.getElement(Env.getCtx(), "AD_User_ID", false));
-	    
-
-	    return columnNames;
-	}
-
+    protected Vector<String> getOISColumnNames() {
+        Vector<String> columnNames = new Vector<>();
+        columnNames.add(Msg.getMsg(Env.getCtx(), "Select"));
+        columnNames.add(Msg.getElement(Env.getCtx(), "AD_Org_ID"));
+        columnNames.add(Msg.translate(Env.getCtx(), "Documentno"));
+        columnNames.add(Msg.translate(Env.getCtx(), "DateRequired"));
+        columnNames.add(Msg.translate(Env.getCtx(), "C_BPartner_ID"));
+        columnNames.add(Msg.getElement(Env.getCtx(), "M_Product_ID", false));
+        columnNames.add(Msg.getElement(Env.getCtx(), "C_Charge_ID", false));
+        columnNames.add(Msg.getElement(Env.getCtx(), "Qty"));
+        columnNames.add(Msg.getElement(Env.getCtx(), "Description", false));
+        columnNames.add(Msg.getElement(Env.getCtx(), "AD_User_ID", false));
+        return columnNames;
+    }
 }
--- src/org/idempiere/faaguilar/webui/form/WCreateFromOrderUI.java
+++ src/org/idempiere/faaguilar/webui/form/WCreateFromOrderUI.java
@@ -32,13 +32,11 @@ import org.adempiere.webui.component.Rows;
 import org.adempiere.webui.editor.WEditor;
 import org.adempiere.webui.editor.WSearchEditor;
 import org.adempiere.webui.editor.WTableDirEditor;
-import org.adempiere.webui.window.FDialog;
 import org.compiere.model.GridTab;
 import org.compiere.model.MColumn;
 import org.compiere.model.MLookup;
 import org.compiere.model.MLookupFactory;
 import org.compiere.model.MRequisition;
-
 import org.compiere.util.CLogger;
 import org.compiere.util.DisplayType;
 import org.compiere.util.Env;
@@ -48,197 +46,178 @@ import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zul.Borderlayout;
 import org.zkoss.zul.Center;
+import org.zkoss.zul.Messagebox;
 
 /**
- * 
  * @author Fabian Aguilar faaguilar@gmail.com
- *
+ * @author Ahmad Fauzi Ridwan @eonebyte (Adapted for iDempiere 13 / ZK 10.x)
  */
-public class WCreateFromOrderUI extends CreateFromOrder implements EventListener<Event>
-{
+public class WCreateFromOrderUI extends CreateFromOrder implements EventListener<Event> {
+
 	private WCreateFromWindow window;
-	
-	public WCreateFromOrderUI(GridTab tab) 
-	{
+	private int p_WindowNo;
+	private CLogger log = CLogger.getCLogger(getClass());
+
+	protected Label requisitionLabel = new Label(Msg.translate(Env.getCtx(), "Requisition"));
+	protected WEditor requisitionLookup;
+
+	protected Label userLabel = new Label(Msg.translate(Env.getCtx(), "User"));
+	protected WEditor userLookup;
+
+	protected Label orgLabel = new Label(Msg.translate(Env.getCtx(), "Org"));
+	protected WTableDirEditor orgLookup;
+
+	public WCreateFromOrderUI(GridTab tab) {
 		super(tab);
 		log.info(getGridTab().toString());
-		
+
 		window = new WCreateFromWindow(this, getGridTab().getWindowNo());
-		
 		p_WindowNo = getGridTab().getWindowNo();
 
-		try
-		{
+		try {
 			if (!dynInit())
 				return;
 			zkInit();
 			setInitOK(true);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			log.log(Level.SEVERE, "", e);
 			setInitOK(false);
 		}
 		AEnv.showWindow(window);
 	}
-	
-	/** Window No               */
-	private int p_WindowNo;
 
-	/**	Logger			*/
-	private CLogger log = CLogger.getCLogger(getClass());
-		
-	protected Label Requisition_Label = new Label(Msg.translate(Env.getCtx(), "Requisition"));
-	protected WEditor requisitionLookup;
-	
-	protected Label User_Label = new Label(Msg.translate(Env.getCtx(), "User"));
-	protected WEditor userLookup;
-	
-	protected Label Org_Label = new Label(Msg.translate(Env.getCtx(), "Org"));
-	protected WTableDirEditor orgLookup;
-	
-	/**
-	 *  Dynamic Init
-	 *  @throws Exception if Lookups cannot be initialized
-	 *  @return true if initialized
-	 */
-	public boolean dynInit() throws Exception
-	{
+	@Override
+	public boolean dynInit() throws Exception {
 		log.config("");
-		
 		super.dynInit();
-		//Refresh button
-				Button refreshButton = window.getConfirmPanel().createButton(ConfirmPanel.A_REFRESH);
-				refreshButton.addEventListener(Events.ON_CLICK, this);
-				window.getConfirmPanel().addButton(refreshButton);				
-		if (getGridTab().getValue("C_Order_ID") == null)
-		{
-			FDialog.error(0, window, "SaveErrorRowNotFound");
+
+		// Tombol Refresh
+		Button refreshButton = window.getConfirmPanel().createButton(ConfirmPanel.A_REFRESH);
+		refreshButton.addEventListener(Events.ON_CLICK, this);
+		window.getConfirmPanel().addButton(refreshButton);
+
+		// FDialog sudah dihapus di iDempiere 13, diganti Messagebox
+		if (getGridTab().getValue("C_Order_ID") == null) {
+			Messagebox.show(
+					Msg.translate(Env.getCtx(), "SaveErrorRowNotFound"),
+					"Error", Messagebox.OK, Messagebox.ERROR);
 			return false;
 		}
-		if (getGridTab().getValueAsBoolean("IsSOTrx"))
-		{
-			FDialog.error(p_WindowNo, window, "Only Purchase Order");
+		if (getGridTab().getValueAsBoolean("IsSOTrx")) {
+			Messagebox.show(
+					"Only Purchase Order is supported.",
+					"Error", Messagebox.OK, Messagebox.ERROR);
 			return false;
 		}
-		
+
 		window.setTitle("Create From Requisition");
-		
-		MLookup lookup = MLookupFactory.get (Env.getCtx(), p_WindowNo, 0, MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_M_Requisition_ID), DisplayType.Search);
-		requisitionLookup = new WSearchEditor ("M_Requisition_ID", false, false, true, lookup);
-		
-		lookup = MLookupFactory.get (Env.getCtx(), p_WindowNo, 0, MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_AD_User_ID), DisplayType.Search);
-		userLookup = new WSearchEditor ("AD_User_ID", false, false, true, lookup);
-		
-		lookup = MLookupFactory.get (Env.getCtx(), p_WindowNo, 0, MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_AD_Org_ID), DisplayType.TableDir);
-		orgLookup = new WTableDirEditor ("AD_Org_ID",false,false,true,lookup);
-		orgLookup.setValue(getGridTab().getValue("AD_Org_ID")); // default
+
+		MLookup lookup = MLookupFactory.get(
+				Env.getCtx(), p_WindowNo, 0,
+				MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_M_Requisition_ID),
+				DisplayType.Search);
+		requisitionLookup = new WSearchEditor("M_Requisition_ID", false, false, true, lookup);
+
+		lookup = MLookupFactory.get(
+				Env.getCtx(), p_WindowNo, 0,
+				MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_AD_User_ID),
+				DisplayType.Search);
+		userLookup = new WSearchEditor("AD_User_ID", false, false, true, lookup);
+
+		lookup = MLookupFactory.get(
+				Env.getCtx(), p_WindowNo, 0,
+				MColumn.getColumn_ID(MRequisition.Table_Name, MRequisition.COLUMNNAME_AD_Org_ID),
+				DisplayType.TableDir);
+		orgLookup = new WTableDirEditor("AD_Org_ID", false, false, true, lookup);
+		orgLookup.setValue(getGridTab().getValue("AD_Org_ID"));
 		orgLookup.getComponent().addEventListener(Events.ON_CHANGE, this);
-		
+
 		return true;
-	}   //  dynInit
-	
-	protected void zkInit() throws Exception
-	{
-		Requisition_Label.setText(Msg.getElement(Env.getCtx(), "M_Requisition_ID"));
-		User_Label.setText(Msg.getElement(Env.getCtx(), "AD_User_ID", false));
-		Org_Label.setText(Msg.getElement(Env.getCtx(), "AD_Org_ID", false));
-        
-        
+	}
+
+	protected void zkInit() throws Exception {
+		requisitionLabel.setText(Msg.getElement(Env.getCtx(), "M_Requisition_ID"));
+		userLabel.setText(Msg.getElement(Env.getCtx(), "AD_User_ID", false));
+		orgLabel.setText(Msg.getElement(Env.getCtx(), "AD_Org_ID", false));
+
 		Borderlayout parameterLayout = new Borderlayout();
 		parameterLayout.setHeight("130px");
 		parameterLayout.setWidth("100%");
-    	Panel parameterPanel = window.getParameterPanel();
+
+		Panel parameterPanel = window.getParameterPanel();
 		parameterPanel.appendChild(parameterLayout);
-		
+
 		Grid parameterStdLayout = GridFactory.newGridLayout();
-    	Panel parameterStdPanel = new Panel();
+		Panel parameterStdPanel = new Panel();
 		parameterStdPanel.appendChild(parameterStdLayout);
 
 		Center center = new Center();
 		parameterLayout.appendChild(center);
 		center.appendChild(parameterStdPanel);
-		
+
 		Columns columns = new Columns();
 		parameterStdLayout.appendChild(columns);
-		Column column = new Column();
-		columns.appendChild(column);		
-		column = new Column();
-		column.setWidth("15%");
-		columns.appendChild(column);
-		column.setWidth("35%");
-		column = new Column();
-		column.setWidth("15%");
-		columns.appendChild(column);
-		column = new Column();
-		column.setWidth("35%");
-		columns.appendChild(column);
-		
-		
+
+		Column col1 = new Column();
+		columns.appendChild(col1);
+		Column col2 = new Column();
+		col2.setWidth("35%");
+		columns.appendChild(col2);
+		Column col3 = new Column();
+		col3.setWidth("15%");
+		columns.appendChild(col3);
+		Column col4 = new Column();
+		col4.setWidth("35%");
+		columns.appendChild(col4);
+
 		Rows rows = (Rows) parameterStdLayout.newRows();
-		Row row = rows.newRow();
-		row.appendChild(Requisition_Label.rightAlign());
-		row.appendChild(requisitionLookup.getComponent());
-		row.appendChild(Org_Label.rightAlign());
-		row.appendChild(orgLookup.getComponent());
-		row = rows.newRow();
-		row.appendChild(User_Label.rightAlign());
-		row.appendChild(userLookup.getComponent());
-		
+
+		Row row1 = rows.newRow();
+		row1.appendChild(requisitionLabel.rightAlign());
+		row1.appendChild(requisitionLookup.getComponent());
+		row1.appendChild(orgLabel.rightAlign());
+		row1.appendChild(orgLookup.getComponent());
+
+		Row row2 = rows.newRow();
+		row2.appendChild(userLabel.rightAlign());
+		row2.appendChild(userLookup.getComponent());
+		row2.appendChild(new Label()); // placeholder
+		row2.appendChild(new Label()); // placeholder
 	}
 
-	/**
-	 *  Action Listener
-	 *  @param e event
-	 * @throws Exception 
-	 */
-	public void onEvent(Event e) throws Exception
-	{
-		if (log.isLoggable(Level.CONFIG)) log.config("Action=" + e.getTarget().getId());
-		if(e.getTarget().equals(window.getConfirmPanel().getButton(ConfirmPanel.A_REFRESH)))
-		{
+	@Override
+	public void onEvent(Event e) throws Exception {
+		if (log.isLoggable(Level.CONFIG))
+			log.config("Action=" + e.getTarget().getId());
+		if (e.getTarget().equals(window.getConfirmPanel().getButton(ConfirmPanel.A_REFRESH))) {
 			loadRequisition();
 			window.tableChanged(null);
 		}
 	}
 
-	/**
-	 *  Load Data - Order
-	 *  @param C_Order_ID Order
-	 *  @param forInvoice true if for invoice vs. delivery qty
-	 */
-	protected void loadRequisition ()
-	{
-		loadTableOIS(getRequisitionData(requisitionLookup.getValue(), orgLookup.getValue(), 
+	protected void loadRequisition() {
+		loadTableOIS(getRequisitionData(
+				requisitionLookup.getValue(),
+				orgLookup.getValue(),
 				userLookup.getValue()));
-	}   //  LoadOrder
-	
-	/**
-	 *  Load Order/Invoice/Shipment data into Table
-	 *  @param data data
-	 */
-	protected void loadTableOIS (Vector<?> data)
-	{
+	}
+
+	protected void loadTableOIS(Vector<?> data) {
 		window.getWListbox().clear();
-		
-		//  Remove previous listeners
 		window.getWListbox().getModel().removeTableModelListener(window);
-		//  Set Model
 		ListModelTable model = new ListModelTable(data);
 		model.addTableModelListener(window);
 		window.getWListbox().setData(model, getOISColumnNames());
-		//
-		
 		configureMiniTable(window.getWListbox());
-	}   //  loadOrder
-	
-	public void showWindow()
-	{
+	}
+
+	@Override
+	public void showWindow() {
 		window.setVisible(true);
 	}
-	
-	public void closeWindow()
-	{
+
+	@Override
+	public void closeWindow() {
 		window.dispose();
 	}
 
